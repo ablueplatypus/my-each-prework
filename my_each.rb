529e6a0d0929fd81ea700c7487a5511e59f1412b
Matt Doering <mattdoering86@gmail.com>
2018-11-08 16:05:19 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array_argument)
+  i = 0
+   while i < array_argument.length
+     yield array_argument[i]
+     i = i + 1
+  end
+  return array_argument
 end
+# my_each([1,2,3,4]) do |value|
+#   "#{value}"
+# end
